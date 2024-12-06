cb43d137bbba4f9ce44d7534996bf54884e866ff
Kartik Hans <21987980+kartikhans@users.noreply.github.com>
2024-12-06 22:35:08 +0530

    Data generation 2.0 (#2)
    * add: new data files added for Nosql and rdbms databases
    
    * gitignore file changed
    
    * del: .env file deleted
    
    ---------
    
    Co-authored-by: Kush Hans <kushhans@kushs-air.mynetworksettings.com>
    Co-authored-by: Kush Hans <kushhans@Kushs-MacBook-Air.local>

--- utils/generate_data_seminar.py
+++ utils/generate_data_seminar.py
@@ -18,15 +18,19 @@ def generate_training_data(client, content):
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-for i in range(15):
-    count = str(random.randint(95, 130))
+for i in range(8, 14):
+    count = str(random.randint(32, 40))
+    # count = 5
+    nested_query = "Identify the top 20 articles with the longest read time from users in different countries, during the last six months, and categorize these by the common top keyword phrases found within user comments."
     if i == 0:
-        content = f"Give {count} natural language queries that can run efficiently on Graph databases"
+        content = f"Give {count} natural language queries that would run efficiently on Non-relational databases like NoSql and very less efficient on other databases also explain why? the query should be a complex like this one {nested_query}"
     else:
-        content = f"Give another {count} unique natural language queries that can run efficiently on Graph databases"
+        content = f"Give another {count} natural language queries that would run efficiently on Non-relational databases like NoSql and very less efficient on other databases also explain why? the query should be a complex like this one {nested_query}"
+
     completion = generate_training_data(client, content)
 
-    with open(f'../data/graph/output_{i}.txt', 'w') as f:
+    # print(completion.choices[0].message.content)
+    with open(f'../data/nosql/query_explaination/output_{i}.txt', 'w') as f:
         f.write(completion.choices[0].message.content)
         f.close()
 
